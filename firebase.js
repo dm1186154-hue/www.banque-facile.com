c7104002c9570020573e72137933d9d6f6ac7d8d
dm1186154-hue <dm1186154@gmail.com>
2026-08-28 21:19:36 +0000

    Configurer Firebase pour Banque Facile
    Ajout de la configuration Firebase et de Firebase Authentication pour permettre la gestion sécurisée des comptes utilisateurs de Banque Facile.
    
    Signed-off-by: dm1186154-hue <dm1186154@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,60 +1,31 @@
-// ===============================
-// BANQUE FACILE - FIREBASE
-// ===============================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
-import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
-
-
-// ===============================
-// CONFIGURATION FIREBASE
-// ===============================
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCedgq5K2ZR_cvvVnbLvUBKwTjAV_Mnc8U",
-  authDomain: "banque-app-66bf9.firebaseapp.com",
-  projectId: "banque-app-66bf9",
-  storageBucket: "banque-app-66bf9.firebasestorage.app",
-  messagingSenderId: "833823730245",
-  appId: "1:833823730245:web:8141ce1171c93040f9912c"
+    apiKey: "AIzaSyCedgq5K2ZR_cvvVnbLvUBKwTjAV_Mnc8U",
+    authDomain: "banque-app-66bf9.firebaseapp.com",
+    projectId: "banque-app-66bf9",
+    storageBucket: "banque-app-66bf9.firebasestorage.app",
+    messagingSenderId: "833823730245",
+    appId: "1:833823730245:web:8141ce1171c93040f9912c"
 };
-
-
-// ===============================
-// INITIALISATION
-// ===============================
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const db = getFirestore(app);
-
-
-// ===============================
-// EXPORT
-// ===============================
 
 export {
-  app,
-  auth,
-  db,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  doc,
-  setDoc,
-  getDoc
+    app,
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
 };
